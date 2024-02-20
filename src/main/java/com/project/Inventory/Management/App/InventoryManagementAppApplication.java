f1d347c7804b9db4e21eca153587c82b58369d0c
deepanshudc <deepanshuchugh.dc@gmail.com>
2024-02-20 22:33:33 +0530

    adding initial files

---
+++ src/main/java/com/project/Inventory/Management/App/InventoryManagementAppApplication.java
@@ -0,0 +1,13 @@
+package com.project.Inventory.Management.App;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InventoryManagementAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InventoryManagementAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/Inventory/Management/App/InventoryManagementAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.Inventory.Management.App;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InventoryManagementAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
